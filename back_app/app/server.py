2f62bd9123f516f70f1296df60baf23ca2ba61c7
Navid <mojtaba.farokhi99@gmail.com>
2023-10-29 22:03:52 +0330

    Enhance exception handling

--- back_app/app/server.py
+++ back_app/app/server.py
@@ -9,11 +9,28 @@ from context.migrations import create_tables
 from cronjobs.fetch_data_job import fetch_data
 from cronjobs.paid_funding_job import calculate_paid_funding
 from namespaces import config_namespace
+from utils.telegram_utils import send_alert, escape_markdown_v1
+
+scheduler: BackgroundScheduler = None
+
+
+def create_schedular():
+    global scheduler
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=fetch_data, trigger="interval", seconds=fetch_data_interval)
+    scheduler.add_job(func=calculate_paid_funding, trigger="interval", seconds=funding_fetch_data_interval)
+    # scheduler.add_job(func=update_binance_deposit, trigger="interval", seconds=update_binance_deposit_interval)
+    scheduler.add_listener(listener, EVENT_JOB_ERROR)
+    scheduler.start()
 
 
 def listener(event):
-    print(f'Job {event.job_id} raised {event.exception.__class__.__name__}')
-    raise event.exception
+    global scheduler
+    send_alert(
+        escape_markdown_v1(
+            f'BackgroundJob {event.job_id} raised {event.exception.__class__.__name__}\n {event.exception}'))
+    scheduler.shutdown(wait=False)
+    create_schedular()
 
 
 app = Flask(__name__)
@@ -24,12 +41,7 @@ api.add_namespace(config_namespace.ns, path="/configs")
 CORS(app, resources={r"*": {"origins": "*"}})
 create_tables()
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=fetch_data, trigger="interval", seconds=fetch_data_interval)
-scheduler.add_job(func=calculate_paid_funding, trigger="interval", seconds=funding_fetch_data_interval)
-# scheduler.add_job(func=update_binance_deposit, trigger="interval", seconds=update_binance_deposit_interval)
-scheduler.add_listener(listener, EVENT_JOB_ERROR)
-scheduler.start()
+create_schedular()
 
 if __name__ == "__main__":
     app.run(port=server_port, debug=True, use_reloader=False)
