e92599faedaebf681919b3272eacdd2666bf6ede
Navid <mojtaba.farokhi99@gmail.com>
2023-10-30 19:29:57 +0330

    Update subgraph source

--- dashboard/src/environments/environment.based.ts
+++ dashboard/src/environments/environment.based.ts
@@ -2,11 +2,11 @@ import {EnvironmentInterface, SubEnvironmentInterface} from "./environment-inter
 
 export const subEnvironment: SubEnvironmentInterface = {
 	name: "Based",
-	subgraphUrl: "https://api.thegraph.com/subgraphs/name/navid-fkh/symmetrical_base",
+	subgraphUrl: "https://api.thegraph.com/subgraphs/name/symmiograph/symmioanalytics_base_8",
 	collateralDecimal: 6,
 	mainColor: "#F5B3F7",
 	accountSource: "0x5dE6949717F3AA8E0Fbed5Ce8B611Ebcf1e44AE9",
-	fromTimestamp: "1692541495",
+	fromTimestamp: "0",
 }
 
 export const environment: EnvironmentInterface = {
