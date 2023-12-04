26029eb6c6a69234baa4ddc1e66c707c492a8e6c
Navid <mojtaba.farokhi99@gmail.com>
2023-12-04 10:02:45 +0330

    Fix aggregate deposit/withdraw amounts

--- dashboard/src/app/utils.ts
+++ dashboard/src/app/utils.ts
@@ -1,28 +1,37 @@
 import {DailyHistory} from "./services/graph-models"
 import BigNumber from "bignumber.js"
 
-export function aggregateDailyHistories(histories: DailyHistory[]): DailyHistory {
-	const keys = [
-		"quotesCount",
-		"tradeVolume",
-		"deposit",
-		"withdraw",
-		"allocate",
-		"deallocate",
-		"activeUsers",
-		"newUsers",
-		"newAccounts",
-		"platformFee",
-		"openInterest"
-	]
-	let base = new DailyHistory()
-	base.id = histories[0].id
-	return histories.reduce((accumulator: DailyHistory, current: DailyHistory) => {
-		for (const key of keys) {
-			const accValue = accumulator[key] as BigNumber || BigNumber(0)
-			const currValue = current[key] as BigNumber
-			accumulator[key] = accValue.plus(currValue)
-		}
-		return accumulator
-	}, base)
+export function aggregateDailyHistories(histories: DailyHistory[], decimalsMap: Map<string, number> | undefined = undefined): DailyHistory {
+    const keys = [
+        "quotesCount",
+        "tradeVolume",
+        "deposit",
+        "withdraw",
+        "allocate",
+        "deallocate",
+        "activeUsers",
+        "newUsers",
+        "newAccounts",
+        "platformFee",
+        "openInterest"
+    ]
+    const withDecimalsKeys = [
+        "deposit",
+        "withdraw",
+    ]
+    let base = new DailyHistory()
+    base.id = histories[0].id
+    return histories.reduce((accumulator: DailyHistory, current: DailyHistory) => {
+        for (const key of keys) {
+            let value = current[key] as BigNumber
+            if (decimalsMap != null && withDecimalsKeys.indexOf(key) >= 0) {
+                const accountSource = current.accountSource!
+                const decimals = decimalsMap.get(accountSource)!
+                value = BigNumber(value!).times(BigNumber(10).pow(18 - decimals))
+            }
+            const accValue = accumulator[key] as BigNumber || BigNumber(0)
+            accumulator[key] = accValue.plus(value as BigNumber)
+        }
+        return accumulator
+    }, base)
 }
