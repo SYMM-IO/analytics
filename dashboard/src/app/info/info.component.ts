3cdb96958a5eac723f3838931f18773c89cd7e09
Navid <mojtaba.farokhi99@gmail.com>
2023-12-05 19:58:16 +0330

    Fix minor problem

--- dashboard/src/app/info/info.component.ts
+++ dashboard/src/app/info/info.component.ts
@@ -1,34 +1,34 @@
-import { LoadingService } from "../services/Loading.service"
-import { Component, Input } from "@angular/core"
+import {LoadingService} from "../services/Loading.service"
+import {Component, Input} from "@angular/core"
 import BigNumber from "bignumber.js"
 
 @Component({
-	selector: "app-info",
-	templateUrl: "./info.component.html",
-	styleUrls: ["./info.component.scss"],
+    selector: "app-info",
+    templateUrl: "./info.component.html",
+    styleUrls: ["./info.component.scss"],
 })
 export class InfoComponent {
-	@Input() title?: string
-	@Input() totalValue?: BigNumber
-	@Input() todayValue?: BigNumber
-	@Input() lastDayValue?: BigNumber
-	@Input() formatMoney: boolean = false
-	@Input() decimals: number = 18
+    @Input() title?: string
+    @Input() totalValue?: BigNumber
+    @Input() todayValue?: BigNumber
+    @Input() lastDayValue?: BigNumber
+    @Input() formatMoney: boolean = false
+    @Input() decimals: number = 18
 
 
-	constructor(readonly loadingService: LoadingService) {
+    constructor(readonly loadingService: LoadingService) {
 
-	}
+    }
 
-	unDecimal(value: BigNumber): BigNumber {
-		return value.div(BigNumber(10).pow(this.decimals))
-	}
+    unDecimal(value: BigNumber): BigNumber {
+        return value.div(BigNumber(10).pow(this.decimals))
+    }
 
 
-	formatValue(value: BigNumber): string {
-		if (this.formatMoney)
-			return this.unDecimal(value).toFormat(3)
-		else
-			return value.toFormat()
-	}
+    formatValue(value: BigNumber): string {
+        if (this.formatMoney)
+            return "$" + this.unDecimal(value).toFormat(3)
+        else
+            return value.toFormat()
+    }
 }
