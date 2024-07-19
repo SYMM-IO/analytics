2ca84fa32fe9a846991de70bacf70e1c162a74bb
naveed <naveedinno@proton.me>
2024-07-19 09:05:19 +0330

    Add pear to aggregate analytics

---
+++ dashboard/src/environments/environment.arbitrum_8_2.ts
@@ -0,0 +1,28 @@
+import {Affiliate, EnvironmentInterface, Hedger} from "./environment-interface"
+
+
+export const pear: Affiliate = {
+	name: "Pear",
+	mainColor: "#ace075",
+	accountSource: "0x6273242a7E88b3De90822b31648C212215caaFE4",
+	fromTimestamp: "0",
+}
+
+export const hedger: Hedger = {
+	name: "arbitrum_Hedger",
+}
+
+export const environment: EnvironmentInterface = {
+	name: "ARBITRUM_8_2",
+	serverUrl: "https://analytics-api.symm.io",
+	subgraphUrl: "https://api.studio.thegraph.com/query/62454/analytics_arbitrum_8_2/version/latest",
+	collateralDecimal: 6,
+	startDate: new Date(1715990400000),
+	affiliates: [
+		pear
+	],
+	hedgers: [
+		hedger,
+	],
+}
+
