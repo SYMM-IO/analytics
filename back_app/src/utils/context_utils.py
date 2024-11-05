775b11e561bd8c619e8215bf9b7a2c8bb3861491
naveed <naveedinno@proton.me>
2024-11-05 16:33:50 +0330

    Move backApp to a separate repo

--- back_app/src/utils/context_utils.py
+++
@@ -1,9 +0,0 @@
-from src.config.local_settings import contexts
-from src.config.settings import Context
-
-
-def get_context(name: str) -> Context | None:
-    for context in contexts:
-        if context.tenant == name:
-            return context
-    return None
